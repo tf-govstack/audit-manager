bc20a430eb117561d67fcdcda083428f84878c33
Sada <sadanandegowda.dm@mindtree.com>
2020-09-25 12:03:03 +0530

    Updated template and template type data

--- db_release_scripts/mosip_audit/sql/1.1.2_audit-scripts_revoke.sql
+++ db_release_scripts/mosip_audit/sql/1.1.2_audit-scripts_revoke.sql
@@ -1,7 +1,7 @@
 -- -------------------------------------------------------------------------------------------------
 -- Database Name: mosip_audit
--- Release Version 	: 1.2.0
--- Purpose    		: Database Alter scripts for the release for Audit DB.       
+-- Release Version 	: 1.1.2
+-- Purpose    		: Revoking Database Alter deployement done for release in Audit DB.       
 -- Create By   		: Sadanandegowda DM
 -- Created Date		: Sep-2020
 -- 
