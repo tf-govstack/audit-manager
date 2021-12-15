a2b8a771e4f82d8e6a6bd85c5e880119d0309f9b
urviljoshi <urvvil08@gmail.com>
2021-12-15 18:35:21 +0530

    after burner added to audit

--- kernel/kernel-auditmanager-service/src/main/java/io/mosip/kernel/auditmanager/exception/AuditAsyncExceptionHandler.java
+++ kernel/kernel-auditmanager-service/src/main/java/io/mosip/kernel/auditmanager/exception/AuditAsyncExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 import io.mosip.kernel.auditmanager.config.AuditManagerLogger;
 
@@ -21,9 +23,7 @@ public class AuditAsyncExceptionHandler implements AsyncUncaughtExceptionHandler
 	/**
 	 * Field for ObjectMapper
 	 */
-	private static final ObjectMapper MAPPER = JsonMapper.builder()
-		    .addModule(new AfterburnerModule())
-		    .build();
+	private static final ObjectMapper MAPPER = JsonMapper.builder().addModule(new AfterburnerModule()).build();
 
 	/*
 	 * (non-Javadoc)
